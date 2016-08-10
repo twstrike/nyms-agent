b9c34d734bc88459822624b28e5a680f03824eee
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-08-10 12:48:55 -0500

    Handle errors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os"
@@ -11,38 +12,44 @@ import (
 
 var pipe bool
 var daemonEnabled bool
-var guiEnabled bool
 var protoDebug bool
 
 func init() {
 	flag.BoolVar(&pipe, "pipe", false, "Run RPC service on stdin/stdout")
 	flag.BoolVar(&daemonEnabled, "d", false, "Run RPC service on unix domain socket")
-	flag.BoolVar(&guiEnabled, "g", false, "Run gui client")
 	flag.BoolVar(&protoDebug, "protoDebug", false, "Log RPC traffic")
 	flag.Parse()
 }
 
 func main() {
-	if pipe {
+	switch true {
+	case pipe:
 		pp := pipes.CreatePipePair(os.Stdin, os.Stdout, protoDebug)
 		defer pp.Close()
 		log.Println("Listening on Stdin ...")
 		for {
 			go pipes.Serve(pp, protoDebug)
 		}
-	} else if daemonEnabled {
+	case daemonEnabled:
 		log.Println("Listening on /tmp/nyms.sock ...")
 		l, err := net.Listen("unix", "/tmp/nyms.sock")
 		if err != nil {
 			log.Fatal(err)
+			return
 		}
+
 		defer l.Close()
 		for {
 			conn, err := l.Accept()
 			if err != nil {
 				log.Fatal(err)
+				continue
 			}
 			go pipes.Serve(conn, protoDebug)
 		}
+	default:
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
 	}
+
 }
