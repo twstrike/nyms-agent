bd3948d8c3039e2d5ea7a2cd2cc5e0535dbd06db
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-08-15 16:14:49 -0500

    Implement HKP index operation

--- hkps/client.go
+++ hkps/client.go
@@ -1,17 +1,27 @@
 package hkps
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
+	"time"
 
 	"golang.org/x/crypto/openpgp"
 	"golang.org/x/crypto/openpgp/armor"
+	"golang.org/x/crypto/openpgp/packet"
 )
 
 type Client interface {
 	Submit(*openpgp.Entity) error
+	Lookup(string) ([]*Index, error)
 }
 
 func translateProtocol(u *url.URL) *url.URL {
@@ -87,4 +97,182 @@ func (c *client) Submit(k *openpgp.Entity) error {
 	// {"inserted":["rsa4096/579ebcb26c9772cdb7a896f297372b211cadf401"],"updated":null,"ignored":null}
 
 	return nil
+}
+
+type Index struct {
+	PrimaryKey
+	UserIDs []UserID
+}
+
+type PrimaryKey struct {
+	KeyId                      uint64
+	Fingerprint                [20]byte
+	PubKeyAlgo                 packet.PublicKeyAlgorithm
+	KeyLen                     int
+	CreationTime               time.Time
+	ExpirationTime             time.Time
+	Disabled, Expired, Revoked bool
+}
+
+type UserID struct {
+	UserID                     string
+	CreationTime               time.Time
+	ExpirationTime             time.Time
+	Disabled, Expired, Revoked bool
+}
+
+func parseIndexes(r io.Reader) ([]*Index, error) {
+	s := bufio.NewScanner(r)
+
+	ret := make([]*Index, 0, 1)
+	for s.Scan() {
+		tokens := strings.SplitN(s.Text(), ":", 7)
+
+		switch tokens[0] {
+		case "info":
+			//stupid. Maybe check version and expand the array?
+		case "pub":
+			index, err := parsePrimaryKey(tokens)
+			if err != nil {
+				return ret, err
+			}
+
+			ret = append(ret, index)
+		case "uid":
+			uid, err := parseUserID(tokens)
+			if err != nil {
+				return ret, err
+			}
+
+			current := ret[len(ret)-1]
+			current.UserIDs = append(current.UserIDs, uid)
+		default:
+			return nil, errors.New("unexpected type:" + tokens[0])
+		}
+	}
+
+	return ret, s.Err()
+}
+
+func parsePrimaryKey(tokens []string) (ret *Index, err error) {
+	if tokens[0] != "pub" {
+		return
+	}
+
+	ret = &Index{}
+
+	if tokens[1] != "" {
+		var bs []byte
+		bs, err = hex.DecodeString(tokens[1])
+		if err != nil {
+			return
+		}
+
+		switch len(bs) {
+		case 8, 4:
+			ret.KeyId = binary.BigEndian.Uint64(bs)
+		case 20:
+			copy(ret.Fingerprint[:], bs)
+		default:
+			//error
+			return
+		}
+	}
+
+	if tokens[2] != "" {
+		var algo int
+		algo, err = strconv.Atoi(tokens[2])
+		if err != nil {
+			return
+		}
+
+		ret.PubKeyAlgo = packet.PublicKeyAlgorithm(algo)
+	}
+
+	if tokens[3] != "" {
+		ret.KeyLen, err = strconv.Atoi(tokens[3])
+		if err != nil {
+			return
+		}
+	}
+
+	ret.CreationTime, err = parseTime(tokens[4])
+	if err != nil {
+		return
+	}
+
+	ret.ExpirationTime, err = parseTime(tokens[5])
+	if err != nil {
+		return
+	}
+
+	if tokens[6] != "" {
+		ret.Revoked = strings.ContainsRune(tokens[6], 'r')
+		ret.Disabled = strings.ContainsRune(tokens[6], 'd')
+		ret.Expired = strings.ContainsRune(tokens[6], 'e')
+	}
+
+	ret.UserIDs = make([]UserID, 0, 0)
+
+	return
+}
+
+func parseTime(in string) (time.Time, error) {
+	var r time.Time
+
+	if in == "" {
+		return r, nil
+	}
+
+	t, err := strconv.ParseInt(in, 10, 64)
+	if err != nil {
+		return r, err
+	}
+
+	return time.Unix(t, 0), nil
+}
+
+func parseUserID(tokens []string) (ret UserID, err error) {
+	if tokens[0] != "uid" {
+		return
+	}
+
+	ret.UserID = tokens[1]
+	ret.CreationTime, err = parseTime(tokens[2])
+	if err != nil {
+		return
+	}
+
+	ret.ExpirationTime, err = parseTime(tokens[3])
+	if err != nil {
+		return
+	}
+
+	if tokens[4] != "" {
+		ret.Revoked = strings.ContainsRune(tokens[4], 'r')
+		ret.Disabled = strings.ContainsRune(tokens[4], 'd')
+		ret.Expired = strings.ContainsRune(tokens[4], 'e')
+	}
+
+	return
+}
+
+func (c *client) Lookup(search string) ([]*Index, error) {
+	p := url.Values{}
+	p.Set("op", "index")
+	p.Set("options", "mr")
+	p.Set("search", search)
+
+	resp, err := http.Get(c.path("/pks/lookup?" + p.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("hkps: unexpected response from server (%s)", resp.Status)
+	}
+
+	return parseIndexes(resp.Body)
 }
--- hkps/client_test.go
+++ hkps/client_test.go
@@ -1,12 +1,17 @@
 package hkps
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"reflect"
+	"strings"
 	"testing"
+	"time"
 
 	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/packet"
 )
 
 var expectedArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
@@ -122,5 +127,169 @@ func TestHKPSSubmit(t *testing.T) {
 	err = c.Submit(e[0])
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
+	}
+}
+
+func TestHKPSIndex(t *testing.T) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/pks/lookup" {
+			t.Errorf("unexpected path: %s", r.URL.Path)
+		}
+
+		if r.Method != "GET" {
+			t.Errorf("unexpected method: %s", r.Method)
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			t.Errorf("unexpected error: %s", err)
+		}
+
+		op := r.Form.Get("op")
+		if op != "index" {
+			t.Errorf("unexpected op: %s", op)
+		}
+
+		options := r.Form.Get("options")
+		if options != "mr" {
+			t.Errorf("unexpected options: %s", options)
+		}
+
+		search := r.Form.Get("search")
+		if search != "nyms-agent" {
+			t.Errorf("unexpected search: %s", search)
+		}
+
+		w.Write([]byte("info:1:1\n"))
+		w.Write([]byte("pub:97372B211CADF401:1:4096:1470692529::\n"))
+		w.Write([]byte("uid:Nyms Agent (for testing purposes) <agent@nyms.io>:1470692529::\n"))
+	}
+
+	s := httptest.NewServer(http.HandlerFunc(fn))
+
+	c, err := NewClient(s.URL)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	entities, err := c.Lookup("nyms-agent")
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if len(entities) != 1 {
+		t.Errorf("unexpected response: %s", entities)
+	}
+
+}
+
+func TestParseIndexesInMachineFormat(t *testing.T) {
+	input := `info:1:1
+pub:97372B211CADF401:1:4096:1470692529::
+uid:Nyms Agent (for testing purposes) <agent@nyms.io>:1470692529::`
+
+	expected := &Index{
+		PrimaryKey: PrimaryKey{
+			KeyId:        0x97372B211CADF401,
+			PubKeyAlgo:   packet.PubKeyAlgoRSA,
+			KeyLen:       4096,
+			CreationTime: time.Unix(1470692529, 0),
+		},
+		UserIDs: []UserID{
+			UserID{
+				UserID:       "Nyms Agent (for testing purposes) <agent@nyms.io>",
+				CreationTime: time.Unix(1470692529, 0),
+			},
+		},
+	}
+
+	r := bytes.NewBufferString(input)
+	entities, err := parseIndexes(r)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if len(entities) != 1 {
+		t.Errorf("unexpected response: %s", entities)
+	}
+
+	if !reflect.DeepEqual(entities[0], expected) {
+		t.Errorf("unexpected response: %#v", entities[0])
+	}
+}
+
+func TestParsePrimaryKey(t *testing.T) {
+	input := "pub:97372B211CADF401:1:4096:1470692529:1470692590:de"
+	tokens := strings.SplitN(input, ":", 7)
+	index, err := parsePrimaryKey(tokens)
+
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if index.KeyId != 0x97372B211CADF401 {
+		t.Errorf("unexpected KeyID: %x", index.KeyId)
+	}
+
+	if index.PubKeyAlgo != packet.PubKeyAlgoRSA {
+		t.Errorf("unexpected PubKeyAlgo: %d", index.PubKeyAlgo)
+	}
+
+	if index.KeyLen != 4096 {
+		t.Errorf("unexpected KeyLen: %d", index.KeyLen)
+	}
+
+	if !index.CreationTime.Equal(time.Unix(1470692529, 0)) {
+		t.Errorf("unexpected CreationTime: %s", index.CreationTime)
+	}
+
+	if !index.ExpirationTime.Equal(time.Unix(1470692590, 0)) {
+		t.Errorf("unexpected ExpirationTime: %s", index.ExpirationTime)
+	}
+
+	if !index.Disabled {
+		t.Errorf("unexpected Disabled: %t", index.Disabled)
+	}
+
+	if !index.Expired {
+		t.Errorf("unexpected Expired: %t", index.Expired)
+	}
+
+	if index.Revoked {
+		t.Errorf("unexpected Revoked: %t", index.Revoked)
+	}
+}
+
+func TestParseUserID(t *testing.T) {
+	input := "uid:Nyms Agent (for testing purposes) <agent@nyms.io>:1470692529:1470692590:rd"
+	tokens := strings.SplitN(input, ":", 7)
+	userID, err := parseUserID(tokens)
+
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+
+	if userID.UserID != "Nyms Agent (for testing purposes) <agent@nyms.io>" {
+		t.Errorf("unexpected UserID: %q", userID.UserID)
+	}
+
+	if !userID.CreationTime.Equal(time.Unix(1470692529, 0)) {
+		t.Errorf("unexpected CreationTime: %s", userID.CreationTime)
+	}
+
+	if !userID.ExpirationTime.Equal(time.Unix(1470692590, 0)) {
+		t.Errorf("unexpected ExpirationTime: %s", userID.ExpirationTime)
+	}
+
+	if !userID.Disabled {
+		t.Errorf("unexpected Disabled: %t", userID.Disabled)
+	}
+
+	if userID.Expired {
+		t.Errorf("unexpected Expired: %t", userID.Expired)
+	}
+
+	if !userID.Revoked {
+		t.Errorf("unexpected Revoked: %t", userID.Revoked)
 	}
 }
