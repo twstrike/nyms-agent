52d5c16aabd01f22561c0314b08454af988219a1
Fan Jiang <fan.torchz@gmail.com>
2016-08-29 12:45:11 -0500

    more example to use pinentry

--- agent/pinentry_test.go
+++ agent/pinentry_test.go
@@ -5,8 +5,13 @@ import "testing"
 func TestPinentry(t *testing.T) {
 	t.Skip("remove me if you want to test pinentry")
 	c := NewPinentryClient("pinentry")
+	defer c.Close()
+	c.SetTitle("Nyms-agent pinentry")
+	c.SetDesc("Nyms-agent asking your passphrase...")
+	c.SetPrompt("PIN please:")
+	c.SetCancel("Wait")
+	c.SetOK("Fine")
 	pin, err := c.GetPin()
-	c.Close()
 	if err != nil {
 		t.Error(err.Error())
 	}
